dc191d15e659e7844f30d45513a6e05a05d374a7
princeampofo <pla252@nyu.edu>
2025-10-01 00:47:22 -0400

    added area for display

--- lib/main.dart
+++ lib/main.dart
@@ -27,14 +27,38 @@ class CalculatorHomePage extends StatefulWidget {
 }
 
 class _CalculatorHomePageState extends State<CalculatorHomePage> {
+
+  // Variable to hold calculation result
+  String _display = '0';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Simple Calculator'),
-        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: const Text('Calculator'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          // Display area
+          Expanded(
+            flex: 2,
+            child: Container(
+              padding: const EdgeInsets.all(16),
+              alignment: Alignment.bottomRight,
+              color: Colors.black87,
+              child: Text(
+                _display,
+                style: const TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
